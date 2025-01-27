13092d18e64c6a880cb32bc9e7a4e683d2499989
Tibor Korsos <tiborkorsos@gmail.com>
2025-01-27 20:06:58 +0100

    plebania-teszt-feltoltes-250127

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+let body = document.querySelector("body");
+let title = document.querySelector(".title");
+let thisYear = document.querySelector("#js-year");
+
+const titleObj = {
+    "plebania": "Plébánia",
+    "templom": "Plébánia, templom, urnatemető",
+}
+
+function titleProcess() {
+    let title = "";
+    for (let k in titleObj) {
+        if(body.dataset.id === k) {
+            title = titleObj[k];
+        }
+    }
+    return title;
+}
+
+function copyYear() {
+    const d = new Date();
+    let year = d.getFullYear();
+    return year;
+}
+
+function render() {
+    title.innerText = titleProcess();
+    thisYear.innerText = copyYear();
+}
+
+render();
